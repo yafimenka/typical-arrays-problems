f3788404b4343c9e9581eac97977c44b58077bb0
yafimenka <fime@bk.ru>
2021-03-03 16:26:40 +0300

    feat: add functions min(), max(), avg() in src\index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (Array.isArray(array)) {
+        if (array.length > 0) {
+            return array.sort((a, b) => a - b)[0];
+        }
+    }
+    return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (Array.isArray(array)) {
+        if (array.length > 0) {
+            return array.sort((a, b) => a - b)[array.length-1];
+        }
+    }
+    return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (Array.isArray(array)) {
+        if (array.length > 0) {
+            return array.reduce((a, b) => a + b, 0) / array.length;
+        }
+    }
+    return 0;
 }
